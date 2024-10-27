69e172b7a96f1c7eb5dc2f727f3c330f1e4c6919
GhetaRares <gheta.rares15@gmail.com>
2024-10-27 16:40:57 +0200

    Teme

---
+++ Tema3.py
@@ -0,0 +1,33 @@
+meniu = ['papanasi'] * 10 + ['ceafa'] * 3 + ["guias"] * 6
+preturi = [["papanasi", 7], ["ceafa", 10], ["guias", 5]]
+studenti = ["Liviu", "Ion", "George", "Ana", "Florica"]
+comenzi = ["guias", "ceafa", "ceafa", "papanasi", "ceafa"]
+tavi = ["tava"] * 7
+istoric_comenzi = []
+print("COMENZI:")
+for student in list(studenti):
+    comanda = comenzi.pop(0)
+    tavi.pop()
+    meniu.remove(comanda)
+    istoric_comenzi.append(comanda)
+    print(f"{student} a comandat {comanda}.")
+print("INVENTAR:")
+numar_guias = istoric_comenzi.count("guias")
+numar_ceafa = istoric_comenzi.count("ceafa")
+numar_papanasi = istoric_comenzi.count("papanasi")
+print(f"Sau comandat {numar_guias} guias, {numar_ceafa} cefe si {numar_papanasi} papanasi")
+print(f"Mai sunt {len(tavi)} tavi.")
+print(f"A mai ramas ceafa: {'ceafa' in meniu} ")
+print(f"Au mai ramas papanasi: {'papanasi' in meniu} ")
+print(f"A mai ramas guias: {'guias' in meniu} ")
+print("FINANTE:")
+incasari = 0
+incasari += preturi[0][1] * numar_papanasi
+incasari += preturi[1][1] * numar_ceafa
+incasari += preturi[2][1] * numar_guias
+print(f"Cantina a încasat: {incasari} lei.")
+produse_ieftine = []
+for produs in preturi:
+    if produs[1] <= 7:
+produse_ieftine.append(produs)
+print(f"Produse mai ieftine de 7 lei: {produse_ieftine}")
